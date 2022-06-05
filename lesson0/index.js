1aaf95b8d3c3d871e7ff9579528236225c780a1f
Daniel Martinez <daniel.martinez.am@gmail.com>
2022-06-05 01:12:47 -0300

    finished intro

---
+++ lesson0/index.js
@@ -0,0 +1,3 @@
+const element = <ul> <li>First</li> <li>Second</li> </ul>
+// ReactDOM.render(<p>Hi, my name is Daniel.</p>, document.getElementById("root"))
+ReactDOM.render(element, document.getElementById("root"))
