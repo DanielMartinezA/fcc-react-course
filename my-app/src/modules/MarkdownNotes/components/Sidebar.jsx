3d159b5b432d9c98e98c98516ef5bbb4c822d965
Daniel Martinez <daniel.martinez.am@gmail.com>
2022-11-30 01:48:54 -0300

    Markdown Notes: first line is used as title for each note.

--- my-app/src/modules/MarkdownNotes/components/Sidebar.jsx
+++ my-app/src/modules/MarkdownNotes/components/Sidebar.jsx
@@ -1,18 +1,24 @@
 import styles from '../MarkdownNotesApp.module.css';
 
+const firstLineRegex = /\n/
+
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                className={`${styles.title} ${
-                    note.id === props.currentNote.id ? styles.selectedNote : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className={styles.textSnippet}>Note {index + 1}</h4>
+    const noteElements = props.notes.map((note, index) => {
+        const newLineIndex = note.body.search(firstLineRegex)
+        return (
+            <div key={note.id}>
+                <div
+                    className={`${styles.title} ${
+                        note.id === props.currentNote.id ? styles.selectedNote : ""
+                    }`}
+                    onClick={() => props.setCurrentNoteId(note.id)}
+                >
+                    <h4 className={styles.textSnippet}>{
+                        newLineIndex === -1 ? note.body : note.body.slice(0, newLineIndex)
+                    }</h4>
+                </div>
             </div>
-        </div>
-    ))
+    )})
 
     return (
         <section className={`${styles.pane} ${styles.sidebar}`}>
