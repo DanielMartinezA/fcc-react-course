3d085709224c316b96ac98e36024d01b82b08e78
Daniel Martinez <daniel.martinez.am@gmail.com>
2022-11-30 17:04:56 -0300

    Deleted unused files. Added setup for Tenzies app.

---
+++ my-app/src/modules/Tenzies/TenziesApp.jsx
@@ -0,0 +1,10 @@
+import styles from './TenziesApp.module.css';
+
+export default function TenziesApp() {
+    
+    return (
+        <main className={styles.mainSection}>
+        main
+        </main>
+    )
+}
